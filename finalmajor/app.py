a7450319987b0888ada90c4a64c1e38350f7b76b
Abdul Suboor <106346130+Asuboor@users.noreply.github.com>
2024-05-28 12:17:44 +0530

    Update app.py

--- finalmajor/app.py
+++ finalmajor/app.py
@@ -154,7 +154,7 @@ def movies():
         for recommendation in recommendations:
             recommendation['Id'] = int(recommendation['Id'])
         response = make_response({"recommendations": recommendations})
-        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        response.headers['Access-Control-Allow-Origin'] = 'https://recomm-ai.vercel.app'
         response.headers['Access-Control-Allow-Methods'] = 'POST, GET'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
 
@@ -198,7 +198,7 @@ def coursera():
         course_name_recommender = CourseRecommender()
         recommendations = course_name_recommender.recommend_ctf(course_name)
         response = make_response({"recommendations": recommendations})
-        response.headers['Access-Control-Allow-Origin'] = 'https://recomm-ai.vercel.app
+        response.headers['Access-Control-Allow-Origin'] = 'https://recomm-ai.vercel.app'
         response.headers['Access-Control-Allow-Methods'] = 'POST','GET'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
 
