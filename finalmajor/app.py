9ed4089cd0cf11a69b4b6ad4b9f66045e0586a8e
Abdul Suboor <106346130+Asuboor@users.noreply.github.com>
2024-05-28 11:27:06 +0530

    Update app.py

--- finalmajor/app.py
+++ finalmajor/app.py
@@ -354,9 +354,7 @@ def format_response(text):
 
 
 
-if __name__ == '__main__':
-    # Change the host parameter to '0.0.0.0' to listen on all network interfaces
-    app.run(host='0.0.0.0', port=5000)
+
 
 
 
